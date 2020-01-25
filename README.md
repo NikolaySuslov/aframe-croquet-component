60608760353f46b73a7f984d13f3e37b6557fb6d
Nikolay Suslov <nsuslovi@gmail.com>
2020-01-26 00:14:20 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# aframe-croquet-component
-A-Frame multi-user Croquet component 
+## A-Frame multi-user Croquet component 
+
+The component allows easily, while staying in [A-Frame](https://aframe.io) scene definition (plain HTML file), to add a multi-user features onto A-Frame entities in scene. Create [Croquet](https://croquet.studio) (aka Croquet V) simulations on A-Frame entities.  
+A-Frame multi-user component works by synchronizing / replicating entities and their components to connected peers using Croquet application architecture. It relaies on public Croquet reflectors, which are avaliable online on the Internet.  
+
+[source code](./public/lib/aframe-croquet-component.js)
+
+<img src="https://krestianstvo.org/sdk/projects/aframe-croquet-component/docs/screen480.gif" width="400">  
+
+[Demo video](https://vimeo.com/387187875)
+
+Getting Started
+---------------
+
+### How to share an entity in an A-Frame scene with other users:  
+
+Add links to this component and croquet lib in the html header 
+```html
+<script src="https://croquet.studio/sdk/croquet-latest.min.js"></script>
+<script src="./lib/aframe-croquet-component.js"></script>
+```
+
+Add `croquet` component to the root <a-scene> element and give a name to the Croquet session
+
+```html
+<a-scene croquet="sessionName: demo-scene">
+```
+
+Give an `id` to the entity (if not exist) and finally add `multiuser` component
+
+```html
+ <a-box id="box1" position="-1.5 1 -3" rotation="0 45 0" color="#4CC3D9" multiuser></a-box>
+``` 
+
+Open the same scene in several Web Browsers windows. The entity should be synced!
+
+
+### How to try out the synchronization:
+
+Open Web Browser Developer Tools and select an entity with `multiuser` component
+```js
+let box = document.querySelector('a-scene').querySelector('#box1')
+```  
+Change entity attributes like, position, rotation, geometry or material etc., by replicating the entity properties
+```js
+box.setAttribute('position', {x:0, y: 1, z: -4});
+box.setAttribute('rotation', {x:0, y: 45, z: 0});
+box.setAttribute('material', {color: '#4BAC41'});
+box.setAttribute('geometry', {width: 3});
+```  
+Start a Croquet application attached to an entity, by replicating a computation (animation example)
+```js
+box.setAttribute('multiuser', {anim: true});
+```
+
+Also you could try to attach to scene the [A-Frame scene Inspector](https://github.com/aframevr/aframe-inspector) by pressing ```<ctrl> + <alt> + i```. Modify entity properties within it's GUI and observe how some of the properties replicates on other peers.
+
+
+Basic Scene Example
+-------------
+[source code](./public/index.html)
+
+```html
+<html>
+<head>
+  <title>A-Frame & Croquet</title>
+  <script src="https://croquet.studio/sdk/croquet-latest.min.js"></script>
+  <script src="https://aframe.io/releases/1.0.3/aframe.min.js"></script>
+  <script src="./lib/aframe-croquet-component.js"></script>
+</head>
+<body>
+  <a-scene croquet="sessionName: demo-scene">
+    <a-box id="box1" position="-1.5 1 -3" rotation="0 45 0" color="#4CC3D9" multiuser="anim: true"></a-box>
+    <a-sphere id="sphere1" position="0 1.25 -5" radius="1.25" color="#EF2D5E" multiuser></a-sphere>
+    <a-cylinder id="cylinder1" position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+    <a-plane id="plane1" position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" multiuser></a-plane>
+    <a-sky color="#ECECEC"></a-sky>
+  </a-scene>
+</body>
+</html>
+```
+
---
+++ public/lib/aframe-croquet-component.js
@@ -0,0 +1,456 @@
+/*
+The MIT License (MIT) 
+Copyright (c) 2020 Nikolai Suslov | Krestianstvo.org
+*/
+
+if (typeof AFRAME === 'undefined') {
+    throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+let Q = Croquet.Constants;
+Q.STEP_MS = 1000 / 20;
+
+class RootModel extends Croquet.Model {
+
+    init(options) {
+        super.init(options);
+        this.children = {};
+        //Aware of Users
+        this.userData = {};
+        this.subscribe(this.sessionId, "view-join", this.addUser);
+        this.subscribe(this.sessionId, "view-exit", this.deleteUser);
+        this.subscribe(this.id, 'onDeleteUser', this.onDeleteUser);
+        this.subscribe(this.id, 'add-multiuser-model', this.onComponentAdd);
+        this.subscribe(this.id, 'delete-multiuser-model', this.onDeleteComponent);
+    }
+
+    newId() {
+        function hex() {
+            let r = Math.random();
+            return Math.floor(r * 256).toString(16).padStart(2, "0");
+        }
+
+        return `${hex()}${hex()}${hex()}${hex()}`;
+    }
+
+    onDeleteComponent(data) {
+
+        let component = this.children[data];
+        if (component) {
+            component.destroy();
+            delete this.children[data];
+        }
+
+    }
+
+    onComponentAdd(data) {
+
+        let elID = data.elID;
+
+        if (!Object.keys(this.children).includes(elID)) {
+            let component = ComponentModel.create(data);
+            this.children[elID] = component;
+            console.log("Model component added!");
+        }
+        this.publish(this.id, 'component-added', elID);
+    }
+
+
+
+    addUser(id) {
+        this.userData[id] = { start: this.now() };
+        console.log(`user ${id} came in`);
+        //this.publish(this.id, 'user-added');
+    }
+
+
+    deleteUser(id) {
+        const time = this.now() - this.userData[id].start;
+        //console.log(`user ${id} left after ${time / 1000} seconds`);
+        //this.publish(this.id, 'onDeleteUser', id);
+    }
+
+    onDeleteUser(id) {
+        if (this.userData[id])
+            delete this.userData[id];
+    }
+
+}
+
+class RootView extends Croquet.View {
+
+    constructor(model) {
+        super(model);
+
+        let self = this;
+
+        this.children = {};
+        this.sceneModel = model;
+        this.aframeScene = document.querySelector('a-scene');
+
+        this.aframeScene.addEventListener('add-multiuser', function (event) {
+            let comp = event.detail.comp;
+            if (!comp.ready) {
+                console.log('Add multiuser component', event.detail);
+                comp.ready = true;
+                let elID = comp.el.id;
+                self.publish(model.id, "add-multiuser-model", { elID: elID });
+            }
+        });
+
+        this.aframeScene.addEventListener('deleteComponent', function (event) {
+
+            let data = event.detail.data;
+            console.log('Delete multiuser component from scene: ', data);
+            self.removeChild(data);
+            self.publish(model.id, 'delete-multiuser-model', data);
+
+        })
+
+        this.subscribe(model.id, 'user-added', this.onUserAdded);
+        this.subscribe(model.id, 'component-added', this.addViewComponent);
+
+    }
+
+    addViewComponent(elID) {
+
+        if (!Object.keys(this.children).includes(elID)) {
+            let component = this.sceneModel.children[elID];
+            let componentView = new ComponentView(component);
+            this.children[elID] = componentView;
+            console.log('View component added!');
+        }
+    }
+
+    onUserAdded() {
+        console.log('User added!')
+    }
+
+    removeChild(childID) {
+        const child = this.children[childID];
+        child.detach();
+        delete this.children[childID];
+    }
+
+    detach() {
+        super.detach();
+        Object.keys(this.children).forEach(key => {
+            this.removeChild(key)
+        })
+    }
+
+}
+
+class ComponentModel extends Croquet.Model {
+
+    init(options) {
+        super.init(options);
+
+        this.components = {
+            multiuser: {
+                anim: false
+            }
+        };
+
+        this.elID = options.elID;
+        this.sceneModel = this.wellKnownModel("modelRoot");
+        this.subscribe(this.id, 'changeComponent', this.changeComponent);
+        this.future(Q.STEP_MS).step();
+
+    }
+
+    step() {
+
+        if (this.components.multiuser.anim) {
+            this.rotate()
+        }
+        this.future(Q.STEP_MS).step();
+
+    }
+
+    rotate() {
+        let t = this.now()
+        let rot = this.components.rotation;
+        if (this.components.rotation) {
+            let newRotation =
+            {
+                rotation:
+                    { x: rot.x, y: Math.sin(t) * 50, z: Math.sin(t) * 30 }
+            };
+            this.changeComponent({ data: newRotation, senderId: 'model' });
+        }
+    }
+
+    merge(target, source) {
+        // Iterate through `source` properties and if an `Object` set property to merge of `target` and `source` properties
+        for (const key of Object.keys(source)) {
+            if (!target[key]) {
+                target[key] = {}
+            }
+            if (source[key] instanceof Object) Object.assign(source[key], this.merge(target[key], source[key]))
+        }
+        // Join `target` and modified `source`
+        Object.assign(target || {}, source)
+        return target
+    }
+
+    changeComponent(changed) {
+        //update model components
+        let diff = JSON.parse(JSON.stringify(changed.data)); //deep object clone
+        this.merge(this.components, changed.data);
+        this.publish(this.id, 'modelChanged', { senderId: changed.senderId, data: diff });
+        //console.log('Model is changed with: ', diff, ' from ', changed.senderId);
+    }
+}
+
+
+class ComponentView extends Croquet.View {
+
+    constructor(model) {
+        super(model);
+
+        let self = this;
+        this.elementModel = model;
+        this.aframeScene = document.querySelector('a-scene');
+        this.aframeEl = this.aframeScene.querySelector('#' + this.elementModel.elID);
+
+        this.aframeEl.addEventListener('setAttribute-event', function (event) {
+
+            let data = event.detail.data;
+            //console.log('Get AFrame setAttribute event with: ', data);
+            self.publish(self.elementModel.id, 'changeComponent', { data: { [data.attrName]: data.value }, senderId: self.viewId });
+
+        })
+
+        this.subscribe(model.id, { event: 'modelChanged', handling: 'oncePerFrame' }, this.changeView);
+        this.subscribe(this.viewId, "synced", this.initView);
+        //this.initView();
+
+    }
+
+    initView() {
+        console.log('init view from model...');
+
+        let modelComponents = this.elementModel.components;
+
+        if (Object.entries(modelComponents).length > 1) {
+            // set from model components
+            this.aframeEl.emit('update-aframe-element', { data: modelComponents });
+        } else {
+            //set up first model from aframe components
+            let newModelComponents = {};
+            let elementComponents = this.aframeEl.components;
+
+            Object.keys(elementComponents).forEach(key => {
+                let prop = this.aframeEl.getAttribute(key);
+                newModelComponents[key] = JSON.parse(JSON.stringify(prop));
+            })
+            this.publish(this.elementModel.id, 'changeComponent', { data: newModelComponents, senderId: this.viewId });
+        }
+    }
+
+    changeView(changed) {
+        this.aframeEl.emit('update-aframe-element', { data: changed.data });
+    }
+
+}
+
+
+ComponentModel.register();
+RootModel.register();
+
+
+AFRAME.registerComponent('croquet', {
+
+    schema: {
+        sessionName: { default: 'sessionName' }
+    },
+
+    init: function () {
+        Croquet.startSession(this.data.sessionName, RootModel, RootView);
+    },
+
+    update: function (oldData) {
+        //TODO: create new user-defined sessions
+    },
+
+    tick: function (t) {
+    }
+})
+
+
+
+AFRAME.registerComponent('multiuser', {
+
+    schema: {
+        anim: { type: 'string', default: false }
+    },
+
+    init: function () {
+        let self = this;
+        this.scene = this.el.sceneEl;
+        this.ready = false;
+
+        Reflect.defineProperty(this.el,
+            'setAttributeAFrame', {
+            value: (this.originalSetAttribute)(),
+            writable: true
+        }
+        )
+
+        Reflect.defineProperty(this.el,
+            'setAttribute', {
+            value: (this.croquetSetAttribute)(),
+            writable: true
+        }
+        )
+
+        this.el.addEventListener('update-aframe-element', function (event) {
+
+            let data = event.detail.data;
+            Object.keys(data).forEach(key => {
+                self.el.setAttributeAFrame(key, data[key]);
+                //console.log('Set attribute on element from model: ', key, ' with: ', data[key])
+            })
+        })
+    },
+
+    //Original defenition from A-Frame master
+    originalSetAttribute: function () {
+        var singlePropUpdate = {};
+        var MULTIPLE_COMPONENT_DELIMITER = '__';
+        var COMPONENTS = AFRAME.components;
+
+        return function (attrName, arg1, arg2) {
+            var newAttrValue;
+            var clobber;
+            var componentName;
+            var delimiterIndex;
+            var isDebugMode;
+            var key;
+
+            delimiterIndex = attrName.indexOf(MULTIPLE_COMPONENT_DELIMITER);
+            componentName = delimiterIndex > 0 ? attrName.substring(0, delimiterIndex) : attrName;
+
+            // Not a component. Normal set attribute.
+            if (!COMPONENTS[componentName]) {
+                if (attrName === 'mixin') { this.mixinUpdate(arg1); }
+                ANode.prototype.setAttribute.call(this, attrName, arg1);
+                return;
+            }
+
+            // Initialize component first if not yet initialized.
+            if (!this.components[attrName] && this.hasAttribute(attrName)) {
+                this.updateComponent(
+                    attrName,
+                    window.HTMLElement.prototype.getAttribute.call(this, attrName));
+            }
+
+            // Determine new attributes from the arguments
+            if (typeof arg2 !== 'undefined' &&
+                typeof arg1 === 'string' &&
+                arg1.length > 0 &&
+                typeof AFRAME.utils.styleParser.parse(arg1) === 'string') {
+                // Update a single property of a multi-property component
+                for (key in singlePropUpdate) { delete singlePropUpdate[key]; }
+                newAttrValue = singlePropUpdate;
+                newAttrValue[arg1] = arg2;
+                clobber = false;
+            } else {
+                // Update with a value, object, or CSS-style property string, with the possiblity
+                // of clobbering previous values.
+                newAttrValue = arg1;
+                clobber = (arg2 === true);
+            }
+
+            // Update component
+            this.updateComponent(attrName, newAttrValue, clobber);
+
+            // In debug mode, write component data up to the DOM.
+            isDebugMode = this.sceneEl && this.sceneEl.getAttribute('debug');
+            if (isDebugMode) { this.components[attrName].flushToDOM(); }
+        };
+    },
+
+    //Modified defenition from A-Frame master
+    croquetSetAttribute: function () {
+        var singlePropUpdate = {};
+        var MULTIPLE_COMPONENT_DELIMITER = '__';
+        var COMPONENTS = AFRAME.components;
+
+        return function (attrName, arg1, arg2) {
+            var newAttrValue;
+            var clobber;
+            var componentName;
+            var delimiterIndex;
+            var isDebugMode;
+            var key;
+
+            delimiterIndex = attrName.indexOf(MULTIPLE_COMPONENT_DELIMITER);
+            componentName = delimiterIndex > 0 ? attrName.substring(0, delimiterIndex) : attrName;
+
+            // Not a component. Normal set attribute.
+            if (!COMPONENTS[componentName]) {
+                if (attrName === 'mixin') { this.mixinUpdate(arg1); }
+                ANode.prototype.setAttribute.call(this, attrName, arg1);
+                return;
+            }
+
+            // Initialize component first if not yet initialized.
+            if (!this.components[attrName] && this.hasAttribute(attrName)) {
+                this.updateComponent(
+                    attrName,
+                    window.HTMLElement.prototype.getAttribute.call(this, attrName));
+            }
+
+            // Determine new attributes from the arguments
+            if (typeof arg2 !== 'undefined' &&
+                typeof arg1 === 'string' &&
+                arg1.length > 0 &&
+                typeof AFRAME.utils.styleParser.parse(arg1) === 'string') {
+                // Update a single property of a multi-property component
+                for (key in singlePropUpdate) { delete singlePropUpdate[key]; }
+                newAttrValue = singlePropUpdate;
+                newAttrValue[arg1] = arg2;
+                clobber = false;
+            } else {
+                // Update with a value, object, or CSS-style property string, with the possiblity
+                // of clobbering previous values.
+                newAttrValue = arg1;
+                clobber = (arg2 === true);
+            }
+
+            // Update component
+            //this.updateComponent(attrName, newAttrValue, clobber);
+            //console.log('Send attribute to model: ', attrName, newAttrValue, clobber);
+            this.emit('setAttribute-event', { data: { attrName: attrName, value: newAttrValue, clobber: clobber } }, false);
+
+
+            // In debug mode, write component data up to the DOM.
+            isDebugMode = this.sceneEl && this.sceneEl.getAttribute('debug');
+            if (isDebugMode) { this.components[attrName].flushToDOM(); }
+        };
+    },
+
+    remove: function () {
+
+        //TODO: remove component and restore AFrame default behaviour
+
+        // Reflect.defineProperty(this.el,
+        //     'setAttribute', {
+        //     value: (this.originalSetAttribute)(),
+        //     writable: true
+        // })
+
+        // this.scene.emit('deleteComponent', { data: this.el.id }, false);
+
+    },
+
+    tick: function (t, dt) {
+
+        if (!this.ready) {
+            this.scene.emit('add-multiuser', { comp: this }, false);
+        }
+
+    }
+})
